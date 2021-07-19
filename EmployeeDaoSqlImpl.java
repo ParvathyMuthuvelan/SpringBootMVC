8894f9df25dc5c579191fadb432a51d495b6391f
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-07-19 16:19:54 +0530

    Add files via upload

--- EmployeeDaoSqlImpl.java
+++ EmployeeDaoSqlImpl.java
@@ -10,6 +10,7 @@ import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import com.bean.Employee;
+
 @Repository
 public class EmployeeDaoSqlImpl extends JdbcDaoSupport implements EmployeeDao{
 	@Autowired 
@@ -19,6 +20,7 @@ public class EmployeeDaoSqlImpl extends JdbcDaoSupport implements EmployeeDao{
 	private void initialize(){
 		setDataSource(dataSource);
 	}
+	
 	@Override
 	public List<Employee> fetchEmployees() {
 		String sql = "SELECT * FROM employees";
@@ -31,10 +33,9 @@ public class EmployeeDaoSqlImpl extends JdbcDaoSupport implements EmployeeDao{
 
 	@Override
 	public void addEmployee(Employee emp) {
-		String sql = "INSERT INTO employee " +
-				"(empId, empName) VALUES (?, ?)" ;
+		String sql = "INSERT INTO employees(firstname,lastname,email) values (?,?,?)" ;
 		getJdbcTemplate().update(sql, new Object[]{
-				emp.getEmployeeId(), emp.getFirstName()
+				emp.getFirstName(), emp.getLastName(),emp.getEmail()
 		});
 		
 	}
@@ -42,19 +43,24 @@ public class EmployeeDaoSqlImpl extends JdbcDaoSupport implements EmployeeDao{
 	@Override
 	public Employee getEmployee(int empId) {
 		Employee emp=new Employee();
-		String sql = "SELECT * FROM employee WHERE empId = ?";
+		String sql = "SELECT * FROM employees WHERE employeeId = ?";
+		emp = getJdbcTemplate().queryForObject(sql, new Object[] { empId }, new EmployeeMapper());
 		return emp;
 	}
 
 	@Override
 	public void updateEmployee(Employee emp) {
-		// TODO Auto-generated method stub
+		String sql = "update employees set firstname=? ,lastname=?,email=? where employeeId=?" ;
+		getJdbcTemplate().update(sql, new Object[]{
+				emp.getFirstName(), emp.getLastName(),emp.getEmail(),emp.getEmployeeId()
+		});
 		
 	}
 
 	@Override
 	public void deleteEmployee(int empId) {
-		// TODO Auto-generated method stub
+		String sql = "delete from employees where employeeId=?" ;
+		getJdbcTemplate().update(sql, new Object[]{	empId	});
 		
 	}
 
